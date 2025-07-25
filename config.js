625481e184228b7eeeca80918bd6a3c2035a9e28
shataa0 <pdgdagma@gmail.com>
2025-07-25 15:53:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -13,14 +13,14 @@ module.exports = {
     CHATBOT: getConfig("CHATBOT") || "on",
     //chatbot on/off
   
-    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~7Y10EarT#NxoOs1kNtMlI6Oj33G3F3KlqHmzcsa_S_n7cr8DsH54",
+    SESSION_ID: process.env.SESSION_ID || "MEGALODON~MD~OEFEFTDT#3zUvW5TiD9rdNAfBuMi_XoRUldizDCSZ72pcFGYCuP8",
     PREFIX: getConfig("PREFIX") || "*",  // Command prefix (e.g., "., / ! * - +")
     BOT_NAME: process.env.BOT_NAME || getConfig("BOT_NAME") || "MEGALODON-MD",  // Bot's display name
     MODE: process.env.MODE || "public",        // Bot mode: public/private/group/inbox
     // ===== OWNER & DEVELOPER SETTINGS =====
     OWNER_NUMBER: process.env.OWNER_NUMBER || "50940210141",  // Owner's WhatsApp number
     
-    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "",           // Owner's name
+    OWNER_NAME: process.env.OWNER_NAME || getConfig("OWNER_NAME") || "D@gma",           // Owner's name
     
     DEV: process.env.DEV || "50948336180",                     // Developer's contact number
     
